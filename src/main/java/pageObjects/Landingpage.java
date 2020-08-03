ea0bf5b4b690560e1628165226a43c5c574e4dba
meeras26 <meeraskocheril@gmail.com>
2020-08-03 11:44:16 +0530

    third testcase that you cannot add more product in cart than the product available in store

--- src/main/java/pageObjects/Landingpage.java
+++ src/main/java/pageObjects/Landingpage.java
@@ -12,11 +12,16 @@ public class Landingpage
 	     this.driver=driver2;
 	}
 By mobile = By.linkText("MOBILE");
+By addtocart=By.cssSelector(".products-grid > li:nth-child(1) > div:nth-child(2) > div:nth-child(4) > button:nth-child(1)");
 
     public WebElement getmobile()
 	{
 		return driver.findElement(mobile);
 	}
+    public WebElement getAddtocart()
+    {
+ 	   return driver.findElement(addtocart);
+    }
     
 
 }
---
+++ src/main/java/pageObjects/Shoppingcartpage.java
@@ -0,0 +1,40 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Shoppingcartpage 
+{
+	public static WebDriver driver;
+	public Shoppingcartpage(WebDriver driver2) 
+	{
+	     this.driver = driver2;
+	}
+	By qty=By.cssSelector(".product-cart-actions > input:nth-child(1)");
+	By update=By.cssSelector("button.btn-update:nth-child(2)");
+	By error=By.xpath("/html/body/div/div/div[2]/div/div/div/form/table/tbody/tr/td[2]/p");
+	By emptycart=By.id("empty_cart_button");
+	By eptymsg=By.xpath("//*[@id=\"top\"]/body/div/div/div[2]/div/div/div[1]/h1");
+	public WebElement getQty()
+	{
+		return driver.findElement(qty);
+	}
+	public WebElement getUpdate()
+	{
+		return driver.findElement(update);
+	}
+	public WebElement getErrormessage()
+	{
+		return driver.findElement(error);
+	}
+	public WebElement getemptycart()
+	{
+		return driver.findElement(emptycart);
+	}
+	public WebElement getemptymesage()
+	{
+		return driver.findElement(eptymsg);
+	}
+
+}
